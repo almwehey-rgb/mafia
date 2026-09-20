86d72e54090609aacf7b03ee41efe255863eab25
almwehey-rgb <almwehey@gmail.com>
2026-09-20 21:51:01 +0300

    Publish stop voice control

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mafia-c248a79856c56383';
+const CACHE = 'mafia-723e8ff3f241a2e8';
 // Warm only the entry point, not megabytes of unused artwork.
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.add('/game.html')));
